5a70a14d8271a95d6bac6f39f99f67a50daf4c3b
Flavio Vinicius Rodrigues de Oliveira <111025683+Flavi2022@users.noreply.github.com>
2025-03-13 19:55:23 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+// Efeito de rolagem suave para links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Botão "Voltar ao Topo"
+window.onscroll = function () {
+    const backToTopButton = document.getElementById('backToTop');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+};
+
+document.getElementById('backToTop').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
